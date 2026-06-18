f3d10f24be9134fe3f46b51539ee8b04a77af406
mahmoud <esam1000hammad@gmail.com>
2026-06-18 19:57:47 +0300

    move content from index to app

--- includes/app.php
+++ includes/app.php
@@ -1,10 +1,17 @@
- <?php
+<?php
 
+ob_start();
 $helpers = ['request','helper', 'AES','routing','db', 'session', 'mail','lang' ,'view'];
 
 foreach ($helpers as $helper) {
       require_once __DIR__ ."/helpers/$helper.php";
 }
+
+session_save_path(config('session.session_save_path'));
+ ini_set('session.gc_probility', 1);
+ session_start([
+        'cookie_lifetime' => config('session.timeout'),
+      ]);
 
  $connect = mysqli_connect(
        config('database.hostname'),
@@ -13,6 +20,15 @@ foreach ($helpers as $helper) {
        config('database.database'),
        config('database.port'),
  );
+
+ if(!$connect) {
+      die("Connection failed: " . mysqli_connect_error());
+ }
+
+
+require_once base_path('/routes/web.php');
+require_once base_path('/includes/exception_error.php');
+
 
 
 
--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <?php
+require_once __DIR__ . "/includes/app.php";
 
- ob_start();
- require_once __DIR__ . "/includes/app.php";
- session_save_path(config('session.session_save_path'));
- ini_set('session.gc_probility', 1);
- session_start([
-     'cookie_lifetime' => config('session.timeout'),
-     ]);
-require_once __DIR__ . "/routes/web.php";
-require_once __DIR__ . "/includes/exception_error.php";
  
-
-  route_init();
+route_init();
      
+
+
+
+
  ob_end_flush();
  mysqli_close($connect);
